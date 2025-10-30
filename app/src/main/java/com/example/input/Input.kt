e66b96716ac6de608bb9606ec7aca549105ea397
ZhilPro27 <zhilalkrisna@gmail.com>
2025-10-30 11:37:34 +0700

    Import set get value

--- app/src/main/java/com/example/input/Input.kt
+++ app/src/main/java/com/example/input/Input.kt
@@ -1,8 +1,10 @@
 package com.example.input
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
