865826e833cc888a9ca86d3b8828c229a243be30
ZhilPro27 <zhilalkrisna@gmail.com>
2025-10-30 11:36:56 +0700

    Membuat variabel untuk mengingat nilai masukan dari keyboard di dalam fungsi FormDataDiri

--- app/src/main/java/com/example/input/Input.kt
+++ app/src/main/java/com/example/input/Input.kt
@@ -1,8 +1,14 @@
 package com.example.input
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 
 @Composable
-fun FormDataDiri(){
-
+fun FormDataDiri(modifier: Modifier){
+    //variable untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 }
